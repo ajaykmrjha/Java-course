a9d2b478f80093856a3a82ce7490bd9445d01224
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-20 23:46:22 +0530

    copy variable myAge to herAge

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -3,7 +3,8 @@ package com.ajaykmrjha;
 public class Main {
 
     public static void main(String[] args) {
-        int age = 30;
-        System.out.println(age);
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println("Her Age: " + herAge);
     }
 }
