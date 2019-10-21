6d72201a00ad42c8884e13e123671e1b4ad9f165
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-21 23:25:00 +0530

    Some useful methods in String class

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         String message = "Hello World" + "!!";
-        System.out.println(message);
+//      Some useful methods defined in String class
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.startsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("!!")); // returns the index of char and word in the given string or return -1 in case not found
+        System.out.println(message.indexOf("lo W"));  // searches for word in the given string
     }
 }
