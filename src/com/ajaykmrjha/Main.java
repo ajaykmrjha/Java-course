46e487a1cc59aec8871f8ab44e76219160c040c3
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-21 17:49:29 +0530

    Third # Primitive Type Variable

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -3,8 +3,16 @@ package com.ajaykmrjha;
 public class Main {
 
     public static void main(String[] args) {
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println("Her Age: " + herAge);
+//        Because byte consume 1 byte of memory and can store value upto -128 to 127 and
+//        int uses 4 byte of memory and can store upto -2B to 2B
+        byte myAge = 30;
+        int viewsCount = 2_123_456_789;
+        long likesCount = 3_123_456_789L;
+        float price = 10.99F;
+        double totalPrice = 12345.7688;
+//        char is for single character
+        char letter = 'A';
+        String message = "Hello World";
+        boolean isEligible = false;
     }
 }
