3a30cfba86ca7972ec286dccf18688c2a35dc9e5
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-21 23:16:06 +0530

    String concatenation example

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -3,10 +3,7 @@ package com.ajaykmrjha;
 public class Main {
 
     public static void main(String[] args) {
-//        Reference type string
-        String message = new String("Hello World");
-//        Shorthand for declaring string reference type
-        String message1 = "Hello World Again";
-        System.out.println(message + ", " + message1);
+        String message = "Hello World" + "!!";
+        System.out.println(message);
     }
 }
