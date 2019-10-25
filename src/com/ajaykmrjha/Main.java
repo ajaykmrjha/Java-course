a99c7f85a62b525ba833cb4b56136c41be2e0e10
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-25 22:03:19 +0530

    Arithmetic Expression : Division

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
 //        Arithmetic Expression
 //        We 5 arithmetic expression in java e.g ( "+", "-", "*", "/", "%")
-        int result = 10 + 3;
-        System.out.println(result);
+        int result = 10 / 3;
+        System.out.println(result); // output will be 3, As in java division of two whole number will be whole number
+
     }
 }
