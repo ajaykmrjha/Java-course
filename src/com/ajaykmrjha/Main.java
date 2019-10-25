e8edf10ba5a901765c152efef8d74b4ea4c6a028
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-25 21:31:31 +0530

    Array Example

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -1,15 +1,17 @@
 package com.ajaykmrjha;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Hello \"Ajay\""; // to escape double quotes
-        System.out.println(message);
-        String windowPath = "c:\\Windows\\..."; // to escape back slash
-        System.out.println(windowPath);
-        String windowPath1 = "c:\nWindows\\..."; // to add new line
-        System.out.println(windowPath1);
-        String windowPath2 = "c:\tWindows\\..."; // to add TAB space
-        System.out.println(windowPath2);
+//        Array example
+        int[] numbers = new int[5];
+//        First ways to initialize array
+        numbers[0] = 1;
+        numbers[1] = 2;
+
+//        Tip: In Java Array is of fix size
+        System.out.println(Arrays.toString(numbers));
     }
 }
