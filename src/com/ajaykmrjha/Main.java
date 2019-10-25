7ad32d682075b0619f0b1d77d15dbf7681e44016
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-25 21:48:09 +0530

    another way to initialize two dimensional array

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -6,8 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 //    Multi-dimensional Array
-        int[][] numbers = new int[2][3];
-        numbers[0][0] = 1;
+        int[][] numbers = { { 1, 3, 2}, {4, 3, 2} };
 //        Tip: To print multi-dimensional use deepToString
 //        deepToString is another method in Array class
         System.out.println(Arrays.deepToString(numbers));
