3e5a9af3114eeb92e9990550417510f8c0679d1f
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-26 21:49:12 +0530

    Math.round | example 1

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -1,15 +1,10 @@
 package com.ajaykmrjha;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-//        Implicit casting
-        String x = "1";
-        int y = Integer.parseInt(x) + 2;  // parseInt is wrapper class for primitive type int
-        System.out.println(y);
-
-
+//        Math.round
+        int result = Math.round(1.1F);
+        System.out.println(result);
     }
 }
