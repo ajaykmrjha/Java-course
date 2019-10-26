de68fb70a55a8e539458292ec19550c579427845
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-26 22:09:23 +0530

    NumberFormat class | Use of getPercentInstance

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -5,8 +5,9 @@ import java.text.NumberFormat;
 public class Main {
 
     public static void main(String[] args) {
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String result = currency.format(1234567.891);
+//        NumberFormat for percent
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        String result = percent.format(0.1);
         System.out.println(result);
     }
 }
