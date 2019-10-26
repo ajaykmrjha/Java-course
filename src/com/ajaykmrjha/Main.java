7032bba9c393006901f010ff91a0f81c6d8ef0eb
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-26 21:29:11 +0530

    Increment operator

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -5,12 +5,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-//        Arithmetic Expression
-//        We 5 arithmetic expression in java e.g ( "+", "-", "*", "/", "%")
-        double result = (double) 10 /  (double) 3;
-//        here "(double) 10 /  (double) 3" is expression
-//        here 10 and 3 is operand and "/" is operator
-        System.out.println(result);
+//        Increment operator
+        int x= 1;
+        x++;
+        System.out.println(x);
 
     }
 }
