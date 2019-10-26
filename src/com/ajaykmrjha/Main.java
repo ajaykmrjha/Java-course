daef654cd727a5fd9194e72e8af8c1d92fb3dd77
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-26 21:40:17 +0530

    Implicit casting : more example

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-//        Implicit casting
-        short x = 1;
-        int y = x + 2;
+//        Implicit casting | when we don't want to lose any data
+//        byte > short > int > long > float > double
+        double x = 1.1;
+        double y = x + 2;
         System.out.println(y);
 
 
