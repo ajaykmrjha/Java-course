2dc29ce979f450eadf22fadbca15b5b43899b74b
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-26 22:15:41 +0530

    Scanner class example

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -1,13 +1,15 @@
 package com.ajaykmrjha;
 
 import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-//        NumberFormat for percent
-
-        String result = NumberFormat.getPercentInstance().format(0.1); // Method chaining
-        System.out.println(result);
+//        Scanner class use to get input from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Age: ");
+        byte age = scanner.nextByte();
+        System.out.println("You are ");
     }
 }
