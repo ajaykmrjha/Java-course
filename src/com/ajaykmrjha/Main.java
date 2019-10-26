7e4807890e5f1ff23dce0df2e10c2421d5b65a0b
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-26 22:00:02 +0530

    Math | Genrates random number from 0 to 100

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 //        Math random method
-        double result = Math.random();
+        int result = (int) Math.round(Math.random() * 100);
         System.out.println(result);
 
     }
