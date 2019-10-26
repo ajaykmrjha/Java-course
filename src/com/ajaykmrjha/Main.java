8d2c85703f6b82093e1fe37b33fde801b829857b
Ajay Jha <Ajay.Jha@idc.bedbath.com>
2019-10-26 21:59:00 +0530

    Math | Genrates random number from 0 to 1

--- src/com/ajaykmrjha/Main.java
+++ src/com/ajaykmrjha/Main.java
@@ -3,16 +3,9 @@ package com.ajaykmrjha;
 public class Main {
 
     public static void main(String[] args) {
-//        Math.round
-        int result = Math.round(1.1F);
-        int result1 = (int) Math.ceil(1.1F);
-        int result2 = (int)Math.floor(1.1F);
-        int maxValue = Math.max(1, 2);
-        int minValue = Math.min(1, 2);
+//        Math random method
+        double result = Math.random();
         System.out.println(result);
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(maxValue);
-        System.out.println(minValue);
+
     }
 }
